dd4f0c9bb34d4308c1926544d6e9feda8ec06073
izokizing <izokizing@gmail.com>
2026-07-14 18:57:06 +0300

    Add DOM element queries and logging

--- dom.js
+++ dom.js
@@ -1,1 +1,11 @@
+const nav = document.querySelector('nav');
+console.log(nav.parentElement);
+console.log(nav.firstElementChild);
+console.log(nav.lastElementChild);
+
+const article = document.querySelector('article');
+console.log(article.nextElementSibling);
+console.log(article.previousElementSibling);
+
+const navLinks = document.querySelectorAll('a');
 
